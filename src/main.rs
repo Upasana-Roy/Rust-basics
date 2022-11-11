a1a905903c82707853e5d247626309f554b08537
Upasana Roy <upasanaroy2002@gmail.com>
2022-11-11 10:39:15 +0000

    Create 10 files

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//main function runs first
+
+
+//fn calculate_weight_on_mars(mass: f32) -> f32 {
+//    let mut gravity: f32 = 10.0;
+//    gravity = 9.89;
+//    (mass * gravity) as f32
+//}
+
+//fn main() {
+//    println!("Hello, world! {}", calculate_weight_on_mars(5.0));
+//}
